082b2f379f14efe316358c6ff68e1207f0924368
Elizaveta.Polyakova <polyakova.elizaveta95@mail.ru>
2022-04-21 00:19:06 +0300

    add new calculations

--- src/test/java/TempClass.java
+++ src/test/java/TempClass.java
@@ -7,13 +7,18 @@ public class TempClass {
     private static void myCycleMethod() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 3; j++) {
-                printMethod(i, j);
+                for (int n = 5; n < 10; n++) {
+                    for (int l = 10; l < 20; l++) {
+                        printMethod(i, j, n, l);
+                    }
+                }
             }
         }
     }
 
-    private static void printMethod(int i, int j) {
+    private static void printMethod(int i, int j, int n, int l) {
         System.out.println("i = " + i + "   k = " + j);
+        System.out.println("p = " + n + "   f = " + l);
         System.out.println("The program is completed!");
     }
 
