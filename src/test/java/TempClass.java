60362e7155d37fb42919d77cb1942b909eaac22f
Aleksandr Butakov <anbngm@gmail.com>
2022-04-20 12:10:22 +0500

    Added the second method

--- src/test/java/TempClass.java
+++ src/test/java/TempClass.java
@@ -4,7 +4,14 @@ public class TempClass {
 
     public static void main(String[] args) {
         myCycleMethod();
+    }
 
+    private static void myCycleMethod() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                printMethod(i, j);
+            }
+        }
     }
 
 }
